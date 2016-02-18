e4182901c260857f0961e6fecd225a7a72dc5533
Helen Guo <benguo@dyn-160-39-124-10.secure.barnard.edu>
2016-02-17 20:30:24 -0500

    pushing my work

--- Helen/week1/voting/app.js
+++
@@ -1,9 +0,0 @@
-var userName=prompt("What's your name");
-alert("hi " + userName);
-var age = prompt("What's your age");
-
-if (age > 18){
-	var choose= prompt("Who are you voting for? Bernie, Hillary, Trump")
-} else{
-	alert("you can't vote :(")
-}
